f47ef39508dcc836b0ae9f243af8d672dea36260
kip <kipyyin@gmail.com>
2017-11-22 22:04:38 +0800

    Add styles to `index.html`

--- fm/scripts/fm.js
+++ fm/scripts/fm.js
@@ -1,20 +1,31 @@
-
-const myButton = document.getElementById('myButton');
+const calculate = document.getElementById('calculate');
 const tarIntRate = document.getElementById('tarIntRate');
+const numberInput = document.querySelectorAll('[type="text"]');
+
+const focusHandler = event => {
+  event.target.className = 'highlight';
+};
+
+const blurHandler = event => {
+  event.target.className = '';
+};
+
+numberInput.forEach( element => element.addEventListener('focus', focusHandler) );
+numberInput.forEach( element => element.addEventListener('blur', blurHandler) );
+
 
 function iconv(intRate, srcComp, tarComp) {
 
     if (srcComp && tarComp && intRate) {
         intRate /= 100
         const convIntRate = tarComp * (Math.pow((1 + intRate/srcComp), (srcComp/tarComp)) - 1);
-        return convIntRate;
+        return convIntRate * 100;
     } else {
-        return intRate;
+        return "Some values are missing!";
     }
 };
 
-
-myButton.addEventListener('click', () => {
+calculate.addEventListener('click', () => {
 
     const intRate = parseFloat(document.querySelector('#intRate').value);
     const srcComp = parseFloat(document.querySelector('#srcComp').value);
